5fd89c5743b2369da2ba35df97a8b4f6e4a47da1
Mohammad Shamma <mohammadshamma@gmail.com>
2012-11-11 20:02:22 -0800

    keep track of how to start an ssl enabled client

--- Main.py
+++ Main.py
@@ -1,8 +1,10 @@
 import sys
-from twisted.internet import reactor
+from twisted.internet import reactor, ssl
 from LockBot import LockBotFactory
 
 if __name__ == "__main__":
     chan = sys.argv[1]
     reactor.connectTCP('localhost', 6667, LockBotFactory('#' + chan))
+    # reactor.connectSSL('localhost', 6668, LockBotFactory('#' + chan),
+    #                    ssl.ClientContextFactory())
     reactor.run()
